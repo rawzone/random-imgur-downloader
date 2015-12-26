3c0c714b0da9ffd7241ff7a89cb4b62bd4eead61
Hans Nielsen <hans@rawz0ne.com>
2015-12-26 21:41:53 +0100

    Updated some formatting and replaced repeating stuff with variable

--- imgurl.py
+++ imgurl.py
@@ -11,24 +11,26 @@ import sys
 # Variables
 numOfPics = 300	# number of pictures to download
 
-imgUrl = "http://i.imgur.com/"
+imgUrl = 'http://i.imgur.com/'
 
-dlPath = "./output/"	# output dir for images
+dlPath = './output/'	# output dir for images
 
-shaSum = "9b5936f4006146e4e1e9025b474c02863c0b5614132ad40db4b925a10e8bfbb9"	# sha256sum of placeholder image from imgur.com
+shaSum = '9b5936f4006146e4e1e9025b474c02863c0b5614132ad40db4b925a10e8bfbb9'	# sha256sum of placeholder image from imgur.com
 
-trys = 1
+tries = 1
 
 # Functions
 def randomnes():	# Function to generate random url to images on imgur.com
 	
-	ext = ".jpg"
+	ext = '.jpg'
+
+	chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 	
-	r1 = random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
-	r2 = random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
-	r3 = random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
-	r4 = random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
-	r5 = random.choice('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789')
+	r1 = random.choice(chars)
+	r2 = random.choice(chars)
+	r3 = random.choice(chars)
+	r4 = random.choice(chars)
+	r5 = random.choice(chars)
 	
 	rT = r1 + r2 + r3 + r4 + r5
 	rTE = rT + ext
@@ -49,50 +51,50 @@ def downloadImages(rTFull, dlPath, filename):	# Download random generatet image 
 	return local_file_name
 
 
-def check_sha256sum(f, shaSum, numOfPics, trys):	# Check sha256sum of downloaded image, check if dead image placeholder downloadet, if so download a replacement image.
+def check_sha256sum(f, shaSum, numOfPics, tries):	# Check sha256sum of downloaded image, check if dead image placeholder downloadet, if so download a replacement image.
 	file_name = f
 	
 	output = subprocess.check_output(['sha256sum', file_name])
 
 	newShaSum = output[:64]	# grap 64 chars of the shasum
-	newShaSum = newShaSum.decode("utf-8")	# converting it to a string to compare to shaSum
+	newShaSum = newShaSum.decode('utf-8')	# converting it to a string to compare to shaSum
 
 
 	# check if new file is a placeholder
 	if newShaSum == shaSum:
-		print ("Try number:", trys, "- dead image placeholder downloaded...")
-		trys += 1
+		print ('Try number:', tries, '- dead image placeholder downloaded...')
+		tries += 1
 
 		# Delete image
 		os.remove(file_name)
 		shortFileName = file_name[9:]
-		print ("deleting file", shortFileName, "and downloading new image...\n")
+		print ('deleting file', shortFileName, 'and downloading new image...\n')
 
 		# Fetch new image
 		rTFull = randomnes()
 		local_file_name = downloadImages(rTFull[0], dlPath, rTFull[1])
-		check_sha256sum(local_file_name, shaSum, numOfPics, trys)
+		check_sha256sum(local_file_name, shaSum, numOfPics, tries)
 		
 	else:
-		print ("Try number:", trys, "- done!\n\n")
+		print ('Try number:', tries, '- done!\n\n')
 
 		return
 
 
 def check_output_dir(f):
 	outputDir = os.path.dirname(f)
-	print ("Checking if output folder exist...")
+	print ('Checking if output folder exist...')
 	if not os.path.exists(outputDir):
-		print ("Output dir not found, making one...\n")
+		print ('Output dir not found, making one...\n')
 		os.makedirs(outputDir)
 	else:
-		print ("output folder exists...\n")
+		print ('output folder exists...\n')
 
 
 # Main
 check_output_dir(dlPath)
-print ("Need to download", numOfPics, "images...\n")
+print ('Need to download', numOfPics, 'images...\n')
 for i in range(numOfPics):
 	rTFull = randomnes()
 	local_file_name = downloadImages(rTFull[0], dlPath, rTFull[1])
-	check_sha256sum(local_file_name, shaSum, numOfPics, trys)
+	check_sha256sum(local_file_name, shaSum, numOfPics, tries)
